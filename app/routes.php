08df1bdfbef20d228de6d948483c0e08178edd5c
Larkin Whitaker <larkin@elkdevelopment.com>
2015-01-23 03:55:29 +0000

    Renamed Dog images with ids

--- app/routes.php
+++ app/routes.php
@@ -1,28 +1,5 @@
 <?php
 
-Route::get('/image-factory', function()
-{
-	$counter = 1;
-	$dirf    = public_path() . "/images/danes";
-	$destinationPath = public_path() . '/images/dogs/';
-	$dir = scandir($dirf);
-
-	foreach($dir as $file) 
-	{
-	   if(($file!='..') && ($file!='.')) 
-	   {
-	      if($counter!=0) 
-	      {
-	      	echo $dirf . '/' . $file . '<br>';
-	      	$image = Image::make(file_get_contents($dirf . '/' . $file));
-	      	$fileName = $counter . '.jpg';
-	      	$image->save($destinationPath . $fileName);
-	      }
-	      $counter++;
-	   }
-	}
-});
-
 /**
  * Show the home page with all of the dogs
  */
